afbeeae778911010d42e29a4079b879704c6da7a
Quan Dong <qxd1617@cs.rit.edu>
2017-10-21 18:53:58 -0400

    Added a few more documentation strings.

--- build_graph.c
+++ build_graph.c
@@ -1,3 +1,10 @@
+/***
+Author: Quan Dong
+Simple main class that allows for the creation of an adjacency list representation of graphs.
+
+*/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "simple_graph.h"
@@ -5,17 +12,19 @@
 int main (void){
 	printf("Reading graph data...\n");
 
-	
-
+	///Create the initial graph to be populated/printed/destroyed
 	ObjectList* graph = ol_create();
+	
+	///Fill the graph with nodes 
 	grh_load_file(graph, stdin);
 	
 
 	printf("Finished reading graph data.\n");
 	
-	
+	///Print out the graph's data
 	grh_print_graph(graph);
 	
+	///Delete the graph
 	grh_delete_graph(graph);
 
 	return 0;
